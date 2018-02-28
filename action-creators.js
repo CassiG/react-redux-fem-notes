d53c645d49f0dd332c198d1975cf0660520e9daa
CassiG <ccgall@gmail.com>
2018-02-28 15:39:55 -0800

    Add additional notes

--- action-creators.js
+++ action-creators.js
@@ -3,6 +3,8 @@ import { SET_SEARCH_TERM } from './actions'
 // you are importing the term SET_SEARCH_TERM from that ceneralized actions area we just created
 // what this does is takes in a search term and then returns a formated action for the reducer which goes into redux reducer and switch on that action type
 // you create a function that takes in the search term and returns an action object.
+// searchTerm is passing in the search term that was created on landing page and handed over to here
+//its creating an object action that will update the searchTerm to this new term
 export function setSearchTerm(searchTerm) {
   return {
     type: SET_SEARCH_TERM,
--- landing.jsx
+++ landing.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { setSearchTerm } from './actionCreators';
 
-const Landing = () => (
+//its getting the props from search tearm redux
+const Landing = (props: { searchTerm: string }) => (
   <div className="landing">
-
+      // This is passing that state into your componenet
+      <input onChange={props.handleSearchTermChange} value={props.searchTerm} type="text" />
   </div>
 )
 
@@ -13,6 +16,16 @@ const Landing = () => (
 // it is assigning state to be the state.searchTerm which is what we difined it to be in our reducers
 const mapStateToProps = (state) => ({ searchTerm: state.searchTerm })
 
+// this creates a method that landing can invoke to dispatch events to redux. it's passing in those action that redux needs to update the state.
+// It gonna return an object
+// it's passing that event.target.valu. this is giving ti to actionCreators
+// This is taking in what has been added to the input box, so taking what the user types and maps it to set search term
+const mapDispatchToProps = (dispatch: Function) => ({
+  handleSearchTermChange(event) {
+    dispatch(setSearchTerm (event.target.value));
+  }
+})
+
 //connect is a function that connects redux store to landing. first is what you're using to pull out state and the second is component.
 // so first you connect your component to your redux store and then you pull out what you want from it.
-export default connect(mapStateToProps)(Landing);
+export default connect(mapStateToProps, mapDispatchToProps)(Landing);
